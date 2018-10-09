99fed757f496c5c418a47c42152f117dae484f55
josuepl <josue.perez.lucero@gmail.com>
2018-10-09 11:35:11 -0500

    Add files via upload

--- hilop - copia.c
+++ hilop - copia.c
@@ -3,10 +3,16 @@
 #include <math.h>
 
 float pot, div; // variables globales 
+int bandera;
 float resultado;
 void potencia(int *argv){
+	if (bandera == 2){
+	sleep(5);
+	}
+	else{
 	float z=1;
 	int i=0;
+	bandera = 1;
 	printf("Thread %x \n",pthread_self());	//Se imprime el Id. del Hilo creado
 	if(argv[2]==0)
 		z = 1;
@@ -18,24 +24,30 @@ void potencia(int *argv){
 	pot = z;
 	resultado = pot;
 	pthread_exit(NULL);	//Finaliza la ejecucion del hilo
-
+	}
 }
 
 void division(int *argv){
-	float w;
-	printf("Thread %x\n",pthread_self());
-	if(argv[2]==0)
-		printf("No se puede realizar operacion\n");	//Se imprime el Id. del Hilo creado
-	else{
-		w=((float)(argv[1])) / ((float)(argv[2]));
-		printf("Divison: %.4f\n",w);
-		div = w;
-		resultado = div;
+	if( bandera == 1){
+	 sleep(5);
+	
 	}
+	else{
+	 bandera = 2 ;
+	 float w;
+	 printf("Thread %x\n",pthread_self());
+	 if(argv[2]==0)
+		printf("No se puede realizar operacion\n");	//Se imprime el Id. del Hilo creado
+	 else{
+		w=((float)(argv[1])) / ((float)(argv[2]));
+		printf("Divison: %.4f\n",w); 
+		div = w;
+ 		resultado = div;
+	 }
 
-	pthread_exit(NULL);	//Finaliza la ejecucion del hilo
+	 pthread_exit(NULL);	//Finaliza la ejecucion del hilo
+	}
 }
-
 int main(int argc, char *argv[]){
 	int i,h[3],j[3];
 	pthread_t th1,th2;	//Direccion donde se almacenaran los identificadores de los hilos
@@ -47,7 +59,9 @@ int main(int argc, char *argv[]){
 	else{
 		for(i=1;i<argc;i++)
 			h[i]=atoi(argv[i]);
-		pthread_create(&th1,NULL,(void*)potencia,&h);
+	if (h[1]< h[2]) bandera = 1;
+	else bandera = 2;
+	pthread_create(&th1,NULL,(void*)potencia,&h);
 	//Se crea el Hilo para ejecutar la funcion Potencia
 
 	pthread_create(&th2,NULL,(void*)division,&h);
