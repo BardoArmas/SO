a25ba2aab7fae1a6e647ec1e2b1169b46e6e0f9b
josuepl <josue.perez.lucero@gmail.com>
2019-06-05 07:32:58 -0500

    Add files via upload

--- padre.c
+++ padre.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main() {
-	int pid;
+	int status;
+	pid_t pid;
 	printf("PADRE: Soy el proceso padre y mi pid es: %d\n", getpid());
+	pid = fork();
+	pid = fork();
 	pid = fork();
 	// En cuanto llamamos a fork se crea un nuevo proceso. En el proceso
 	// padre 'pid' contendrá el pid del proceso hijo. En el proceso hijo
@@ -13,21 +18,26 @@ int main() {
 	{
 		printf("PADRE: Soy el proceso padre y mi pid sigue siendo: %d\n", getpid());
 		printf("PADRE: Mi hijo tiene el pid: %d\n", pid);
-		int pid2;
+		pid_t pid2;
+		waitpid(pid,&status,0);
 		pid2=fork();
 		if(pid2==0){//codigo hijo B
 			printf("HIJOB: Soy el proceso hijo B con pid: %d\n",getpid());
 			printf("HIJOB: mi padre tiene el pid: %d\n",getppid());
+			sleep(5);
 		}
+
 		else{//codigo del padre
+			waitpid(pid2,&status,0);
 			printf("Soy el proceso padre con pid: %d\n",getpid());
 			printf("Mi hijoB tiene el pid: %d\n ",pid2);
-			sleep(10);
+			sleep(5);
 		}
 
 	}
 	else // Proceso hijo A
 	{
+		sleep(5);
 		printf("HIJO: Soy el proceso hijo y mi pid es: %d\n", getpid());
 		printf("HIJO: mi padre tiene el pid: %d\n", getppid());
 	}
